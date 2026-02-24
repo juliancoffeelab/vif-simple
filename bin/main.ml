358b6c4c08d81c262be6eebb5ca389c7cfe9692d
juliancoffee <lightdarkdaughter@gmail.com>
2026-02-24 16:23:46 +0200

    Add Vif

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,17 @@
-let () = print_endline "Hello, World!"
+open Vif
+
+let default req _server () =
+  let open Vif.Response.Syntax in
+  let str = "Hello World!\n" in
+  let* () = Response.with_string req str in
+  Response.respond `OK
+;;
+
+let routes =
+  let open Vif.Uri in
+  let open Vif.Route in
+  [ get (rel /?? nil) --> default ]
+;;
+
+let () = Miou_unix.run @@ fun () ->
+  Vif.run routes ()
